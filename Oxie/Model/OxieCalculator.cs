5e1ecd17f5e091b6689d4006be4788255b9769b1
alexesprit <alex.esprit@gmail.com>
2018-02-06 19:45:23 +0300

    Update oxygen and propane calculation

--- Oxie/Model/OxieCalculator.cs
+++ Oxie/Model/OxieCalculator.cs
@@ -9,12 +9,12 @@ namespace Oxie.Model {
         public float OxygenUsage {
             get {
                 if (oxygen.ContainsKey(Thickness)) {
-                    return oxygen[Thickness] * CuttingLength / 1000;
+                    return oxygen[Thickness] * CuttingLength / 1000 * 1.33f;
                 }
                 throw new InvalidThicknessError();
             }
         }
-        public float PropaneUsage { get {
+        public float PropaneUsage {get {
                 if (propane.ContainsKey(Thickness)) {
                     return propane[Thickness] * CuttingLength / 1000;
                 }
@@ -27,12 +27,22 @@ namespace Oxie.Model {
             { 8, 0.199f },
             { 10, 0.242f },
             { 12, 0.283f },
+            { 14, 0.376f },
+            { 16, 0.398f },
+            { 20, 0.431f },
+            { 25, 0.546f },
+            { 30, 0.608f }
         };
         private Dictionary<int, float> propane = new Dictionary<int, float>() {
             { 6, 0.032f },
             { 8, 0.041f },
             { 10, 0.048f },
             { 12, 0.058f },
+            { 14, 0.066f },
+            { 16, 0.071f },
+            { 20, 0.084f },
+            { 25, 0.098f },
+            { 30, 0.106f }
         };
     }
 }
